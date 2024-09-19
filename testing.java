cb3abfc4f0f248f0f999b3c82496f43b6ca3e00c
VivaanAmin842076 <842076@pdsb.net>
2024-09-19 12:06:11 -0400

    Create testing.java

---
+++ testing.java
@@ -0,0 +1,8 @@
+class Testing {
+      public static void main(String[] args) {
+        double circ;
+        double rad = 3;
+        circ = 2 * Math.PI * rad;
+        System.out.println("The circumference of a circle of radius " + rad + " is " + circ);
+      }
+}

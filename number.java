8c6f9fdce958d572ccdc218ef5175a5223568915
VivaanAmin842076 <842076@pdsb.net>
2024-09-19 12:08:19 -0400

    Create number.java

---
+++ number.java
@@ -0,0 +1,6 @@
+class number {
+  public static void main(String[] args) {
+        int numberOfKeys = 102;
+        System.out.println("The number of keys on a keyboard is " + numberOfKeys);
+    }
+}

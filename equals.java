d36feb7032a1d93e2c9ef1fd78b94ad03849184a
VivaanAmin842076 <842076@pdsb.net>
2024-09-19 12:55:47 -0400

    Create equals.java

---
+++ equals.java
@@ -0,0 +1,10 @@
+
+public class equals {
+    public static void main(String[] args) {
+    int amount = 24;
+    System.out.println(amount);
+    System.out.println("Two dozen is: " + amount);
+    int amount1 = 7 * 4;
+    System.out.println("Twenty-eight is: " + amount1);
+    }
+}

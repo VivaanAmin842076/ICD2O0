715ec774ebf9ba835c22e6b24bc69a18bec61a1d
VivaanAmin842076 <842076@pdsb.net>
2024-10-29 12:19:17 -0400

    Create U5L1.java

---
+++ U5L1.java
@@ -0,0 +1,16 @@
+/* 
+ * Debugging L1: Numbers
+ */
+
+class Main {
+
+    public static void main(String[] args) {
+       int num1 = 6;
+        int num2 = 4;
+
+        int result = num1 * num2;
+        
+        System.out.println(num1 * num2);
+    }
+
+}

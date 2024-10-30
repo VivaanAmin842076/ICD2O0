0843a8d6f1f980b1df5a37505c0f1a194c06cfe5
VivaanAmin842076 <842076@pdsb.net>
2024-10-30 13:41:53 -0400

    Create U5L5.java

---
+++ U5L5.java
@@ -0,0 +1,12 @@
+class U5L5 {
+    public static void main(String[] args) {
+        // Declare variables to store the type of transport and the number of wheels
+        String transport = "unicycle";
+        int wheels = 1;
+
+        // Display the type of transport and the number of wheels
+        System.out.print("A " + transport + " has " + wheels + " wheel.");
+
+        ///////////// END of tasks ///////////////////
+    }
+}

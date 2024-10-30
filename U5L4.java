133fe1739ac8e26a68d15d32a9afee52ad8b1a19
VivaanAmin842076 <842076@pdsb.net>
2024-10-30 13:41:20 -0400

    Create U5L4.java

---
+++ U5L4.java
@@ -0,0 +1,11 @@
+class U5L4 {
+    public static void main(String[] args) {
+        // Declare and initialize the variable
+        String lastMonth = "August";
+
+        // Use the correct method to print the string
+        System.out.printf("The last month was %s.\n", lastMonth);
+
+        //////////// End of tasks /////////////////////
+    }
+}

573615d65a98366026eb4c6d2cc2b14331c6a01b
VivaanAmin842076 <842076@pdsb.net>
2024-11-13 12:22:39 -0500

    Create U6L3.java

---
+++ U6L3.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class marks {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Please enter a percentage mark (0-100): ");
+        int mark = scanner.nextInt();
+
+       
+        if (mark < 0 || mark > 100) {
+            System.out.println("Invalid input. Please enter a mark between 0 and 100.");
+        } else {
+            
+            if (mark >= 80) {
+                System.out.println("Your letter grade is: A");
+            } else if (mark >= 70) {
+                System.out.println("Your letter grade is: B");
+            } else if (mark >= 60) {
+                System.out.println("Your letter grade is: C");
+            } else if (mark >= 50) {
+                System.out.println("Your letter grade is: D");
+            } else {
+                System.out.println("Your letter grade is: F");
+            }
+        }
+
+      
+        scanner.close();
+    }
+}
